2b3283513eaafa7b8d801a46a4cd8c9b31e6754a
KseniyaDobina <dobinakseniya@gmail.com>
2026-08-01 21:35:59 +0300

    Небольшие исправления ошибок

--- app_vaccines/models/schemas.py
+++ app_vaccines/models/schemas.py
@@ -2,7 +2,21 @@ from pydantic import BaseModel
 
 
 class Vaccine(BaseModel):
-    title: str
-    description: str | None = None
+    disease: str
+    vaccine_name: str
+    # vaccination_date: date
+    # expiration_date: date | None = None
+    clinic: str
+    country: str
+    city: str
+    notes: str | None = None
+
+class VaccineUpdate(BaseModel):
+    disease: str | None = None
+    vaccine_name: str | None = None
+    # vaccination_date: date
+    # expiration_date: date | None = None
+    clinic: str | None = None
     country: str | None = None
     city: str | None = None
+    notes: str | None = None
--- app_vaccines/routers/vaccines.py
+++ app_vaccines/routers/vaccines.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, HTTPException, Depends
+from pydantic import BaseModel
+
 from models.schemas import Vaccine
-import depends
+from routers import depends
 
 router = APIRouter(
     prefix="/vaccines",
@@ -25,7 +27,7 @@ def get_vaccines(vaccine_id: int):
             return {"message": f"Информация о вакцине №{vaccine_id}",
                     "vaccine": vaccine}
 
-    return HTTPException(status_code=404, detail="Данные о вакцинации не найдены")
+    raise HTTPException(status_code=404, detail="Данные о вакцинации не найдены")
 
 @router.post("")
 def create_vaccines(vaccine: Vaccine):
@@ -34,7 +36,7 @@ def create_vaccines(vaccine: Vaccine):
     :param vaccine: вакцина
     :return: 201 если создана, в других случаях ошибку
     """
-    new_vaccine = dict(vaccine)
+    new_vaccine = vaccine.model_dump()
     new_vaccine["id"] = len(fake_database) + 1
     fake_database.append(new_vaccine)
     return {"message": f"Добавлена вакцина {new_vaccine['id']}",
@@ -47,25 +49,25 @@ def put_vaccines(vaccine_id: int, vaccine: Vaccine):
     :param vaccine_id: id вакцины
     :return:
     """
-    for idx, vaccine in enumerate(fake_database):
-        if vaccine["id"] == vaccine_id:
-            updated_vaccine = dict(vaccine)
+    for idx, vaccine_count in enumerate(fake_database):
+        if vaccine_count["id"] == vaccine_id:
+            updated_vaccine = vaccine.model_dump()
             updated_vaccine["id"] = vaccine_id
             fake_database[idx] = updated_vaccine
             return {"message": f"Полностью зменены данные о вакцине №{vaccine_id}",
                     "vaccine": updated_vaccine}
 
-    return HTTPException(status_code=404, detail="Данные о вакцинации не найдены")
+    raise HTTPException(status_code=404, detail="Данные о вакцинации не найдены")
 
 @router.patch("/{vaccine_id}")
 def patch_vaccines(vaccine_id: int):
     return {"message": f"Частично изменены данные о вакцине №{vaccine_id}"}
 
 @router.delete("/{vaccine_id}")
-def delete_vaccines(vaccine_id: int, vaccine: Vaccine):
+def delete_vaccines(vaccine_id: int):
     for idx, vaccine in enumerate(fake_database):
         if vaccine["id"] == vaccine_id:
             del fake_database[idx]
             return {"message": f"Удалена вакцина №{vaccine_id}"}
 
-    return HTTPException(status_code=404, detail="Данные о вакцинации не найдены")
+    raise HTTPException(status_code=404, detail="Данные о вакцинации не найдены")
