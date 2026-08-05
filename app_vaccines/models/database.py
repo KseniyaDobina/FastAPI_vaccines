995ef2fa7e668119fdea73198cd35e91664d16eb
KseniyaDobina <dobinakseniya@gmail.com>
2026-08-05 21:27:53 +0300

    Изменила путь до базы данных

--- app_vaccines/models/database.py
+++ app_vaccines/models/database.py
@@ -2,7 +2,8 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 from app_vaccines.models.db_models import Base
 
-engine = create_async_engine('sqlite+aiosqlite:///vaccines_db.sqlite3')
+PATH_TO_DB = "sqlite+aiosqlite:///data/vaccines_db.sqlite3"
+engine = create_async_engine(PATH_TO_DB)
 new_session = async_sessionmaker(engine, expire_on_commit=False)
 
 async def create_database():
