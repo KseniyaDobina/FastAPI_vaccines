9ce738ac23294a3544128d2ccac4a35dea167cd1
KseniyaDobina <dobinakseniya@gmail.com>
2026-08-05 21:27:53 +0300

    Изменила путь до базы данных

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
+from pathlib import Path
 import os
 
-load_dotenv()
 
-BASE_URL = os.getenv("BASE_URL")
+load_dotenv()
+PATH_TO_DB = os.getenv("PATH_TO_DB")
+print(PATH_TO_DB, 67)
