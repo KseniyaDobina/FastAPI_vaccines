a98c3cdc7a92743c6c6e0d291cac326c17cdd7bd
KseniyaDobina <dobinakseniya@gmail.com>
2026-08-05 21:31:06 +0300

    Добавила docker-compose.yml

--- app_vaccines/models/database.py
+++ app_vaccines/models/database.py
@@ -2,6 +2,7 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 from app_vaccines.models.db_models import Base
 
+# Потом надо убрать как переменную окружения
 PATH_TO_DB = "sqlite+aiosqlite:///data/vaccines_db.sqlite3"
 engine = create_async_engine(PATH_TO_DB)
 new_session = async_sessionmaker(engine, expire_on_commit=False)
