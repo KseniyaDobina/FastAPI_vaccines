460bbd0ee2fcb9c4082d80ee612df4accf59cf0c
KseniyaDobina <dobinakseniya@gmail.com>
2026-08-24 23:37:08 +0300

    Добавила новые поля в БД

--- app_vaccines/models/db_models.py
+++ app_vaccines/models/db_models.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import date
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
@@ -9,10 +9,17 @@ class Base(DeclarativeBase):
 class VaccineBase(Base):
     __tablename__ = 'vaccines'
     id: Mapped[int] = mapped_column(primary_key=True)
+    # user: Mapped[str]
+    # date_created_note: Mapped[date]
+    # date_change_note: Mapped[date]
     disease: Mapped[str]
     vaccine_name: Mapped[str]
-    # vaccination_date: Mapped[datetime]
-    # expiration_date: Mapped[datetime | None]
+    dose_number: Mapped[str]
+    vaccination_date: Mapped[date]
+    expiration_date: Mapped[date | None]
+    type_vaccine: Mapped[str]
+    lot: Mapped[str]
+    manufacturer: Mapped[str]
     clinic: Mapped[str]
     country: Mapped[str]
     city: Mapped[str]
--- app_vaccines/models/schemas.py
+++ app_vaccines/models/schemas.py
@@ -1,10 +1,18 @@
+from datetime import date
 from pydantic import BaseModel, ConfigDict, Field
 
 class VaccineCreate(BaseModel):
+    # user: str = Field(min_length=3, max_length=100)
+    # date_created_note: date
+    # date_change_note: date
     disease: str = Field(min_length=3, max_length=100)
     vaccine_name: str = Field(min_length=3, max_length=100)
-    # vaccination_date: date
-    # expiration_date: date | None = None
+    dose_number: str = Field(min_length=1, max_length=30)
+    vaccination_date: date
+    expiration_date: date | None = None
+    type_vaccine: str = Field(min_length=1, max_length=100)
+    lot: str = Field(min_length=1, max_length=100)
+    manufacturer: str = Field(min_length=2, max_length=100)
     clinic: str = Field(min_length=3, max_length=200)
     country: str = Field(min_length=2, max_length=100)
     city: str = Field(min_length=2, max_length=100)
@@ -15,10 +23,17 @@ class VaccineID(VaccineCreate):
     model_config = ConfigDict(from_attributes=True)
 
 class VaccineUpdate(BaseModel):
+    # user: str = Field(min_length=3, max_length=100)
+    # date_created_note: date
+    # date_change_note: date
     disease: str | None = Field(default=None, min_length=3, max_length=100)
     vaccine_name: str | None = Field(default=None, min_length=3, max_length=100)
-    # vaccination_date: date
-    # expiration_date: date | None = None
+    dose_number: str = Field(min_length=1, max_length=30)
+    vaccination_date: date
+    expiration_date: date | None = None
+    type_vaccine: str = Field(min_length=1, max_length=100)
+    lot: str = Field(min_length=1, max_length=100)
+    manufacturer: str = Field(min_length=2, max_length=100)
     clinic: str | None = Field(default=None, min_length=3, max_length=200)
     country: str | None = Field(default=None, min_length=2, max_length=100)
     city: str | None = Field(default=None, min_length=2, max_length=100)
