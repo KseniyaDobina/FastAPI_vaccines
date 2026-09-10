7b09dc09547acb6403a30eb646acbe047afb19f4
KseniyaDobina <dobinakseniya@gmail.com>
2026-09-10 21:45:27 +0300

    Убрала из всех запросов API проверку пользователя в отдельную функцию

--- app_vaccines/routers/vaccines.py
+++ app_vaccines/routers/vaccines.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter, HTTPException, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app_vaccines.auth.dependencies import get_current_user
 from app_vaccines.models.database import get_session
-from app_vaccines.models.repository import VaccineRepository, VaccineService, UserRepository
-from app_vaccines.models.schemas import VaccineCreate, VaccineUpdate, VaccineID, MessageAPIResponse, CurrentUser
+from app_vaccines.models.repository import VaccineRepository, VaccineService
+from app_vaccines.models.schemas import VaccineCreate, VaccineUpdate, VaccineID, MessageAPIResponse
 from app_vaccines.routers import depends
 
 router = APIRouter(
@@ -17,14 +16,10 @@ router = APIRouter(
 async def get_all_vaccines(
         session: AsyncSession = Depends(get_session),
         pagination: dict = Depends(depends.pagination_parameters),
-        current_user: CurrentUser = Depends(get_current_user)):
+        user_id: int = Depends(depends.get_current_user_id)):
     """
     Получение списка всех вакцин
     """
-    user_id = await UserRepository.get_user(current_user, session)
-
-    if user_id is None:
-        raise HTTPException(status_code=401, detail="Пользователь не создан в сервисе, нужно его создать в users")
 
     skip = pagination["skip"]
     limit = pagination["limit"]
@@ -36,15 +31,11 @@ async def get_all_vaccines(
 @router.post("", status_code=status.HTTP_201_CREATED, response_model=VaccineID)
 async def create_vaccine(
         vaccine: VaccineCreate,
-        current_user: CurrentUser = Depends(get_current_user),
+        user_id: int = Depends(depends.get_current_user_id),
         session: AsyncSession = Depends(get_session)):
     """
     Создание записи о новой вакцинации
     """
-    user_id = await UserRepository.get_user(current_user, session)
-
-    if user_id is None:
-        raise HTTPException(status_code=401, detail="Пользователь не создан в сервисе, нужно его создать в users")
 
     new_vaccine = await VaccineService.add_vaccine(vaccine, user_id, session)
 
@@ -54,15 +45,11 @@ async def create_vaccine(
 @router.get("/{vaccine_id}", response_model=VaccineID)
 async def get_vaccine(
         vaccine_id: int,
-        current_user: CurrentUser = Depends(get_current_user),
+        user_id: int = Depends(depends.get_current_user_id),
         session: AsyncSession = Depends(get_session)):
     """
     Поиск вакцинации по id
     """
-    user_id = await UserRepository.get_user(current_user, session)
-
-    if user_id is None:
-        raise HTTPException(status_code=401, detail="Пользователь не создан в сервисе, нужно его создать в users")
 
     vaccine = await VaccineService.get_vaccine_by_id(vaccine_id, user_id, session)
 
@@ -76,15 +63,11 @@ async def get_vaccine(
 async def put_vaccine(
         vaccine_id: int,
         vaccine: VaccineCreate,
-        current_user: CurrentUser = Depends(get_current_user),
+        user_id: int = Depends(depends.get_current_user_id),
         session: AsyncSession = Depends(get_session)):
     """
     Обновление информации о вакцинации
     """
-    user_id = await UserRepository.get_user(current_user, session)
-
-    if user_id is None:
-        raise HTTPException(status_code=401, detail="Пользователь не создан в сервисе, нужно его создать в users")
 
     new_vaccine_db = await VaccineService.update_vaccine(vaccine_id, vaccine, user_id, session)
 
@@ -98,16 +81,11 @@ async def put_vaccine(
 async def patch_vaccine(
         vaccine_id: int,
         vaccine: VaccineUpdate,
-        current_user: CurrentUser = Depends(get_current_user),
+        user_id: int = Depends(depends.get_current_user_id),
         session: AsyncSession = Depends(get_session)):
     """
     Обновление определенной информации о вакцине, можно указать только конкретное поле
     """
-    user_id = await UserRepository.get_user(current_user, session)
-
-    if user_id is None:
-        raise HTTPException(status_code=401, detail="Пользователь не создан в сервисе, нужно его создать в users")
-
     updated_vaccine = await VaccineService.update_vaccine(vaccine_id, vaccine, user_id, session)
 
     if updated_vaccine is None:
@@ -119,15 +97,11 @@ async def patch_vaccine(
 @router.delete("/{vaccine_id}", response_model=MessageAPIResponse)
 async def delete_vaccine(
         vaccine_id: int,
-        current_user: CurrentUser = Depends(get_current_user),
+        user_id: int = Depends(depends.get_current_user_id),
         session: AsyncSession = Depends(get_session)):
     """
     Удаление записи о вакцинации
     """
-    user_id = await UserRepository.get_user(current_user, session)
-
-    if user_id is None:
-        raise HTTPException(status_code=401, detail="Пользователь не создан в сервисе, нужно его создать в users")
 
     result = await VaccineRepository.delete_vaccine(vaccine_id, user_id, session)
 
