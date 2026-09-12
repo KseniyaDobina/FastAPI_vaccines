70acac65cc1a89cb580f6e0520f30e1a7e926875
KseniyaDobina <dobinakseniya@gmail.com>
2026-09-13 00:56:58 +0300

    Исправила название теста

--- tests/users/test_unauthenticated.py
+++ tests/users/test_unauthenticated.py
@@ -11,7 +11,7 @@ from tests.config import client, test_db
     ],
 )
 @pytest.mark.asyncio
-async def test_vaccines_endpoints_without_auth(client, method, url):
+async def test_users_endpoints_without_auth(client, method, url):
     response = await getattr(client, method)(url)
 
     assert response.status_code == 401
